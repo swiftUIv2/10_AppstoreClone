229cc25d5e5c1d89cee19d26007456c034325377
emm <723125@gmail.com>
2021-01-11 14:21:32 +0000

    Initial Commit

---
+++ AppstoreClone_10/AppstoreClone_10App.swift
@@ -0,0 +1,17 @@
+//
+//  AppstoreClone_10App.swift
+//  AppstoreClone_10
+//
+//  Created by emm on 11/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct AppstoreClone_10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppstoreClone_10/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppstoreClone_10
+//
+//  Created by emm on 11/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
